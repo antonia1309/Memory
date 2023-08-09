deb43f52542e89514cf142327249ffecb0e44f10
Antonia <familleravel@hotmail.fr>
2023-08-09 22:35:06 +0200

    gestion bug js et mise en forme css

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
+/*
+gerer l aleatoire ds le placement des cartes
+
+*/
+
+// déclarer un tableau de toutes les cartes
 let allCards = document.querySelectorAll(".card")/*on va aller choper ttes les classes card */
 let cptClickCurrent = 0; /*nbre de click */
-let dataImageShowed; 
+let CardClickedId; 
 
+//j ajoute l evenement de click sur ttes les cartes
 allCards.forEach(card =>{
-  card.addEventListener("click", function (){
-    playGame(card); /*chq fois qu on clique sur une carte on lance la fonction playgame */
+    card.addEventListener("click", function (){
+        clickOnCardEvent(card); /*chq fois qu on clique sur une carte on lance la fonction clickOnCardEvent */
   });
 });
 
-function playGame(card){
-   cptClickCurrent  ++;/* est ce qu on a cliqué 1 ou 2 fois */
+//cette fonction gère ce qui se passe qd on click sur une carte
+function clickOnCardEvent(card){
+   if(card.classList.contains("finded")){
+      return; /*ca v dire tu ne fait pas cette fonction */
+   }
+  
+   cptClickCurrent  ++;/* cpte le nbre de click dc au 1er ou au 2e on cache ou montre l image */
   
    // premier click, je cache les images trouvées avant 
     if(cptClickCurrent == 1){
@@ -25,33 +37,37 @@ function playGame(card){
       // j'affiche la carte que je viens de cliquer
       card.classList.remove("hidden");/*on enlève le hidden*/
       //je stocke la réponse derrière la carte
-      dataImageShowed = card.dataset.image;/*pr recup un data attribut avec dataset ;.image du data attributs ds html */
+      CardClickedId = card.id;/*on va stocker l id de l image du html */
     }
     else if(cptClickCurrent == 2){
       //deuxième click, je verifie si l'image a été trouvée
-        card.classList.remove("hidden");
-        if(dataImageShowed == card.dataset.image){
-          allCards.forEach(card =>{
-            if(card.classList.contains("hidden")){
-              // c'est une carte cachée
+      
+      /*ici on gère le bug du click deux fois sur la mm image: */
+        if(CardClickedId == card.id){ /*on verifie si l image cliqué n a pas la mm id */
+            cptClickCurrent =1;/*Si l image ok donc on remet à 1 le click comme si j avais pas cliqué */
+            return;
+        }
+      
+        else{
+            card.classList.remove("hidden");
+            let cardClickedBefore = document.getElementById(CardClickedId)
+            if(cardClickedBefore.dataset.image == card.dataset.image){ /*ici on compare l'image qu on a recup ds le html */
+              allCards.forEach(card =>{
+                if(card.classList.contains("hidden")){
+                  // c'est une carte cachée
+                }
+                else{ /* sinon c une carte ouverte et on va ajouter une class finded*/
+                  card.classList.add("finded");
+                }
+              });
             }
-            else{ /* sinon c une carte ouverte et on va ajouter une class finded*/
-              card.classList.add("finded");
-            }
-            });
-          }
-        
-        cptClickCurrent = 0;
-        dataImageShowed = "";/*Pr sauvegarder la 1ere carte trouvée pr comparer au 2e et après on repart à 0 d'où vide*/
-  }
+            
+            cptClickCurrent = 0;
+            CardClickedId = "";/*Pr sauvegarder la 1ere carte trouvée pr comparer au 2e et après on repart à 0 d'où vide*/
+        }
+    }     
 } 
   
 
 
-  /*if(card.classList.contains("hidden")){ //les cards qui contiennent la class hidden 
-      card.classList.remove("hidden");//on enlève le hidden
-    }
-    else{
-      card.classList.add("hidden");//sinon on l'ajoute 
-    } 
-}*/
+  
